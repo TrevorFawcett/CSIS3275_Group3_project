5a3c9503b1ef6d884bad20adc4335fcca8500776
Carlos Quintero <quinterogordilloc@student.douglascollege.ca>
2023-10-13 21:43:31 -0700

    createde the loan service class

---
+++ src/main/java/com/csis3275/loan/model/LoanServiceImpl.java
@@ -0,0 +1,24 @@
+package com.csis3275.loan.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.csis3275.loan.repositories.ILoanRepository;
+
+@Service
+public class LoanServiceImpl {
+
+	@Autowired
+	ILoanRepository loanService;
+	
+	
+	//CREATE
+	
+	//READ
+	
+	//READ BY ID
+	
+	//GET ALL BALANCES 
+	
+	//GET ALL BALANCES BY ID
+}
