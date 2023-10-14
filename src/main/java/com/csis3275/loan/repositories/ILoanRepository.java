c3f3b7b4087c7434ffef7b8feefa2484f4998cb0
Carlos Quintero <quinterogordilloc@student.douglascollege.ca>
2023-10-13 23:27:03 -0700

    added method that executes query and retrieves records

--- src/main/java/com/csis3275/loan/repositories/ILoanRepository.java
+++ src/main/java/com/csis3275/loan/repositories/ILoanRepository.java
@@ -1,10 +1,18 @@
 package com.csis3275.loan.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+
 import com.csis3275.loan.model.Loan_group3;
 
 @Repository
 public interface ILoanRepository extends CrudRepository<Loan_group3, Long>{
+	
+	@Query("SELECT l FROM Credit_group3 l WHERE l.user.id = :userId")
+	List<Loan_group3> findAllById(Long userId);
 
 }
