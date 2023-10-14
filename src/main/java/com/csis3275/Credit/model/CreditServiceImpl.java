3575b8a515df9971141c5713e86710ba683b4d71
Carlos Quintero <quinterogordilloc@student.douglascollege.ca>
2023-10-13 20:06:27 -0700

    create method to get balance by Id

--- src/main/java/com/csis3275/Credit/model/CreditServiceImpl.java
+++ src/main/java/com/csis3275/Credit/model/CreditServiceImpl.java
@@ -8,40 +8,43 @@ import org.springframework.stereotype.Service;
 import com.csis3275.Credit.Repositories.ICreditRepository;
 import com.csis3275.Credit.model.Credit_group3;
 
-
 @Service
 public class CreditServiceImpl {
-	
-	
+
 	@Autowired
 	private ICreditRepository creditService;
-	
-	
-	//CREATE
-	public Credit_group3 createCreditCardAccounts(Credit_group3 newCreditCard)	{
+
+	// CREATE
+	public Credit_group3 createCreditCardAccounts(Credit_group3 newCreditCard) {
 		return creditService.save(newCreditCard);
 	}
-	
-	//READ
-	public List<Credit_group3> readCreditCardAccounts()	{
-		return (List<Credit_group3>)creditService.findAll();
+
+	// READ
+	public List<Credit_group3> readCreditCardAccounts() {
+		return (List<Credit_group3>) creditService.findAll();
 	}
-	
-	
-	//READ BY ID
-		public List<Credit_group3> readCreditCardAccountsById(Long Id)	{
-			return (List<Credit_group3>)creditService.findAllById(Id);
-		}
-	
-	
-	
-    // Get all account balances
-    public List<Float> getAllAccountBalances() {
-        List<Credit_group3> accounts = (List<Credit_group3>) creditService.findAll();
-        List<Float> accountBalances = accounts.stream()
-                .map(Credit_group3::getBalance) // Extract the balance of each account
-                .toList();
-        return accountBalances;
-    }
+
+	// READ BY ID
+	public List<Credit_group3> readCreditCardAccountsById(Long Id) {
+		return (List<Credit_group3>) creditService.findAllById(Id);
+	}
+
+	// Get all account balances by Id
+	public List<Float> getAllAccountBalancesById(Long Id) {
+		List<Credit_group3> accounts = (List<Credit_group3>) creditService.findAllById(Id);
+		List<Float> accountBalances = accounts.stream().map(Credit_group3::getBalance) // Extract the balance of each
+																						// account
+				.toList();
+		return accountBalances;
+	}
+
+	// Get all account balances
+	public List<Float> getAllAccountBalances() {
+		List<Credit_group3> accounts = (List<Credit_group3>) creditService.findAll();
+		List<Float> accountBalances = accounts.stream().map(Credit_group3::getBalance) // Extract the balance of each
+																						// account
+				.toList();
+		return accountBalances;
+	}
 
 }
