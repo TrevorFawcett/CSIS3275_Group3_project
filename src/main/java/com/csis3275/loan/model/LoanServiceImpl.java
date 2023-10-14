0984c39d83093e8cc8b42f0645532c4edf259cfd
Carlos Quintero <quinterogordilloc@student.douglascollege.ca>
2023-10-13 21:52:37 -0700

    added methods to the loan service class

--- src/main/java/com/csis3275/loan/model/LoanServiceImpl.java
+++ src/main/java/com/csis3275/loan/model/LoanServiceImpl.java
@@ -1,8 +1,11 @@
 package com.csis3275.loan.model;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.csis3275.loan.model.Loan_group3;
 import com.csis3275.loan.repositories.ILoanRepository;
 
 @Service
@@ -13,12 +16,27 @@ public class LoanServiceImpl {
 	
 	
 	//CREATE
+	public Loan_group3 loans(Loan_group3 newLoan)	{
+		return loanService.save(newLoan);
+	}
 	
 	//READ
+	public List<Loan_group3> readCreditCardAccounts()	{
+		return (List<Loan_group3>)loanService.findAll();
+	}
+	
 	
 	//READ BY ID
 	
 	//GET ALL BALANCES 
+	
+	public List<Double> getAllAccountBalances() {
+        List<Loan_group3> accounts = (List<Loan_group3>) loanService.findAll();
+        List<Double> accountBalances = accounts.stream()
+                .map(Loan_group3::getTotalToBePaid) // Extract the balance of each account
+                .toList();
+        return accountBalances;
+    }
 	
 	//GET ALL BALANCES BY ID
 }
