530a6e56e442914f92a23f2bc1f7ea0439a3e88b
Carlos Quintero <quinterogordilloc@student.douglascollege.ca>
2023-10-13 23:51:46 -0700

    fix: forgot to add the user object to the loan object - SOLVED

--- src/main/java/com/csis3275/loan/Controller/LoanController.java
+++ src/main/java/com/csis3275/loan/Controller/LoanController.java
@@ -3,18 +3,25 @@ package com.csis3275.loan.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.csis3275.loan.model.LoanServiceImpl;
 import com.csis3275.loan.model.Loan_group3;
+import com.csis3275.login.model.User_group3;
+import com.csis3275.login.service.UserServiceImpl_group3;
 
 @Controller
 public class LoanController {
 	
 	@Autowired
 	private LoanServiceImpl loanService;
+	
+	@Autowired 
+	private UserServiceImpl_group3 userService;
 
 	@GetMapping("/user-page/loan/add")
 	public String addLoan(Model model) {
@@ -25,21 +32,39 @@ public class LoanController {
 	@PostMapping("/user-page/loan/add")
 	public String addNewLoan(Loan_group3 loanToAdd) {
 		
-		float payment;    
+		
+Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
+		if (authentication != null) {
+			
+			String username = authentication.getName();
+			User_group3 currentUser = userService.getUserByEmail(username); 
+			
+			
+			loanToAdd.setUser(currentUser);
+			
+			float payment;    
 
-		if (loanToAdd.getLoan_type().equals("Car")) {
-			loanToAdd.setRate(0.03f);
+			if (loanToAdd.getLoan_type().equals("Car")) {
+				loanToAdd.setRate(0.03f);
+
+				
+			} else if (loanToAdd.getLoan_type().equals("Home")) {
+				loanToAdd.setRate(0.075f);
+
+			}
+			
+			
+			payment = (loanToAdd.getAmount() * (loanToAdd.getRate() * loanToAdd.getLoanTerm() ) );
+			
+			loanToAdd.setTotalToBePaid(payment + loanToAdd.getAmount());
 
 			
-		} else if (loanToAdd.getLoan_type().equals("Home")) {
-			loanToAdd.setRate(0.075f);
-
+		
+		
+		
 		}
 		
-		
-		payment = (loanToAdd.getAmount() * (loanToAdd.getRate() * loanToAdd.getLoanTerm() ) );
-		
-		loanToAdd.setTotalToBePaid(payment + loanToAdd.getAmount());
 
 
 		loanService.createLoans(loanToAdd);
