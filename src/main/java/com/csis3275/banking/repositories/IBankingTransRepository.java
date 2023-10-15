f88f31e55074850fd2c4fe00927eeb3d63194f64
Carlos Quintero <quinterogordilloc@student.douglascollege.ca>
2023-10-15 03:32:15 -0700

    added banking trans repository

---
+++ src/main/java/com/csis3275/banking/repositories/IBankingTransRepository.java
@@ -0,0 +1,9 @@
+package com.csis3275.banking.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.csis3275.banking.model.BankingTrans_group3;
+
+public interface IBankingTransRepository extends CrudRepository<BankingTrans_group3,Long>{
+
+}
