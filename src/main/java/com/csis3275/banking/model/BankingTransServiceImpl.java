e2ff5ac93976a0ccca09468ee1469bc1356edff0
Carlos Quintero <quinterogordilloc@student.douglascollege.ca>
2023-10-15 03:51:50 -0700

    created banking transactions service

---
+++ src/main/java/com/csis3275/banking/model/BankingTransServiceImpl.java
@@ -0,0 +1,40 @@
+package com.csis3275.banking.model;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.csis3275.banking.repositories.IBankingTransRepository;
+
+@Service
+public class BankingTransServiceImpl {
+	
+	
+	
+	@Autowired
+	IBankingTransRepository bankingTransService;
+	
+	
+	//CREATE
+	public BankingTrans_group3 createBankingTransaction(BankingTrans_group3 newBankingTransaction) {
+		return bankingTransService.save(newBankingTransaction);
+	}
+
+	
+	//READ
+	
+	public List<BankingTrans_group3> readBankingTransactions() {
+		return (List<BankingTrans_group3>) bankingTransService.findAll();
+	}
+	
+	
+	//READ BY ID
+	
+	public List<BankingTrans_group3> readBankingTransactionsById(Long Id) {
+		return bankingTransService.findAllById(Id);
+	}
+	
+	
+	
+}
