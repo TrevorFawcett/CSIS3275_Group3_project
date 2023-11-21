944a604873037db8985b63e2234ab8e42b540ebf
Carlos Quintero <quinterogordilloc@student.douglascollege.ca>
2023-11-21 06:19:53 -0800

    refactored the controller

--- src/main/java/com/csis3275/investment/InvestController_Cqu_81.java
+++ src/main/java/com/csis3275/investment/InvestController_Cqu_81.java
@@ -1,39 +1,55 @@
 package com.csis3275.investment;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+
+import com.csis3275.Credit.model.CreditServiceImpl;
+import com.csis3275.Credit.model.CreditTransServiceImpl;
+import com.csis3275.Credit.model.CreditTrans_group3;
+import com.csis3275.Credit.model.Credit_group3;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+
 @Controller
 public class InvestController_Cqu_81 {
+	
+	@Autowired 
+	private CreditServiceImpl creditService;
+	
+	@Autowired
+	CreditTransServiceImpl creditTransService;
 
 
-    @Value("${alphavantage.apikey}")
-    private String apiKey;
+    private String apiKey = "ZYM7CUCE7B1VJIIX";
     private final RestTemplate restTemplate = new RestTemplate();
+    public long CreditID;
 
     @GetMapping("/invest-page")
-    public String investPage(Model model) {
+    public String investPage(Model model, @RequestParam("credit_id") Long Id) {
         model.addAttribute("symbol", "");
         model.addAttribute("name", "");
         model.addAttribute("exchange", "");
         model.addAttribute("currency", "");
         model.addAttribute("data", new ArrayList<>());
         model.addAttribute("cashFlow", new ArrayList<>());
+        model.addAttribute("creditId", Id);
         return "investment";
     }
 
     @SuppressWarnings("unchecked")
     @PostMapping("/invest-page")
     public String fetchData(@RequestParam String symbol, Model model) {
+    	
         try {
             // Fetching overview data
             String overviewUrl = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=" + symbol + "&apikey=" + apiKey;
@@ -60,5 +76,26 @@ public class InvestController_Cqu_81 {
 
         model.addAttribute("symbol", symbol);
         return "investment";
+    }
+    
+    
+    @PostMapping("/invest/add/{creditId}")
+    public String invest(@RequestParam String price, Model model, @PathVariable Long creditId) {
+    		
+    	
+    		float Price = Float.parseFloat(price);
+    		
+    		CreditTrans_group3 newTran = new CreditTrans_group3();
+    		Credit_group3 credit = creditService.readSingleCreditAccount(creditId);
+    		newTran.setCredit(credit);
+    		newTran.setType("Purchase");
+    		newTran.setDescription("Investment");
+    		credit.setBalance((float) (credit.getBalance() + Price));
+    		newTran.setAmount(Price);
+    		newTran.setRefunded(false);
+    		creditTransService.createCreditTransaction(newTran);
+    		
+
+        return "redirect:/user-page";
     }
 }
