ad08dc0dd2ef309475f80e6c8695b9d4228b0cbb
Carlos Quintero <quinterogordilloc@student.douglascollege.ca>
2023-12-04 21:50:39 -0800

    chore: created the banking Transaction logic for the banking transfers in the bankingTransService class

--- src/main/java/com/csis3275/banking/model/BankingTransServiceImpl.java
+++ src/main/java/com/csis3275/banking/model/BankingTransServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.csis3275.banking.repositories.IBankingTransRepository;
+import com.csis3275.banking.model.Banking_group3;
 
 
 
@@ -21,6 +22,35 @@ public class BankingTransServiceImpl {
 	//CREATE
 	public BankingTrans_group3 createBankingTransaction(BankingTrans_group3 newBankingTransaction) {
 		return bankingTransService.save(newBankingTransaction);
+	}
+	
+	
+	//save transfer transaction
+	public BankingTrans_group3 createBankingTransactionTransfer(String senderOrReciever, float amount, Banking_group3 banking) {
+		
+		BankingTrans_group3 trans = new BankingTrans_group3();
+		
+		if (senderOrReciever == "sender") {
+			
+			trans.setType("withdrawal");
+			trans.setWithdrawal(amount);
+			trans.setDeposit(0.0f);
+			banking.setBalance((float) (banking.getBalance() - amount));
+
+		}else {										
+			trans.setType("deposit");
+			trans.setWithdrawal(0.0f);
+			trans.setDeposit(amount);
+			banking.setBalance((float) (banking.getBalance() + amount));
+			
+		}
+		
+		trans.setAmount(amount);
+		trans.setDescription("MONEY TRANSFER");
+		trans.setBanking(banking);
+		trans.setRefunded(false);
+		
+		return bankingTransService.save(trans);
 	}
 	
 	
