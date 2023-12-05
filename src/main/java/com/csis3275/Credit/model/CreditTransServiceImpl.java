dda88842cc8b82993e9ebbb440683e2163a97a67
Carlos Quintero <quinterogordilloc@student.douglascollege.ca>
2023-12-04 21:51:12 -0800

    chore: created the credit Transaction logic for the credit transfers in the creditTransService class

--- src/main/java/com/csis3275/Credit/model/CreditTransServiceImpl.java
+++ src/main/java/com/csis3275/Credit/model/CreditTransServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.csis3275.Credit.Repositories.ICreditTransRepository;
 import com.csis3275.banking.model.BankingTrans_group3;
+import com.csis3275.banking.model.Banking_group3;
 
 @Service
 public class CreditTransServiceImpl {
@@ -21,6 +22,29 @@ public class CreditTransServiceImpl {
 	public CreditTrans_group3 createCreditTransaction(CreditTrans_group3 newCreditTransaction) {
 		return creditTransService.save(newCreditTransaction);
 	}
+	
+	//save transfer transaction
+		public CreditTrans_group3 createCreditTransactionTransfer(String senderOrReciever, float amount, Credit_group3 credit) {
+			
+			CreditTrans_group3 trans = new CreditTrans_group3();
+			if (senderOrReciever == "sender") {
+				
+				trans.setType("withdrawal");
+				credit.setBalance((float) (credit.getBalance() + amount));
+				trans.setDescription("MONEY TRANSFER");
+
+			}else {										
+				trans.setType("deposit");
+				credit.setBalance((float) (credit.getBalance() - amount));
+				trans.setDescription("PAYMENT");
+				
+			}
+			trans.setAmount(amount);
+			trans.setCredit(credit);
+			trans.setRefunded(false);
+			
+			return creditTransService.save(trans);
+		}
 	
 	
 	//READ BY ONE
